b499e8a6affc8bd2c9fa2d977571e95bf7cd07c1
vitalikshu <37334718+vitalikshu@users.noreply.github.com>
2021-02-26 19:05:00 +0200

    first

--- Restourant/ContentView.swift
+++ Restourant/ContentView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct Restourant: Identifiable {
+struct Restaurant: Identifiable {
     let id = UUID()
     let name: String
     let address: String
 }
 
-struct RestourantRow:View {
-    let restourant: Restourant
+struct RestaurantRow:View {
+    let restaurant: Restaurant
     var body: some View{
         VStack(alignment: .leading){
-            Text(restourant.name).font(.callout).foregroundColor(Color(#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)))
-            Text(restourant.address).font(.subheadline).foregroundColor(.gray)
+            Text(restaurant.name).font(.callout).foregroundColor(Color(#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)))
+            Text(restaurant.address).font(.subheadline).foregroundColor(.gray)
         }
     }
 }
@@ -26,10 +26,10 @@ struct RestourantRow:View {
 struct ContentView: View {
     var body: some View {
         NavigationView{
-        let restourants = [Restourant(name: "Название первого ресторана", address: "Адресс первого ресторана"),Restourant(name: "Названте второго ресторана", address: "Адресс второго ресторана")]
+        let restaurants = [Restaurant(name: "Название первого ресторана", address: "Адресс первого ресторана"),Restaurant(name: "Название второго ресторана", address: "Адресс второго ресторана")]
         
-        List(restourants){ restourant in
-            RestourantRow(restourant: restourant)
+        List(restaurants){ restaurant in
+            RestaurantRow(restaurant: restaurant)
         }
         .navigationBarTitle("Все рестораны")
         .font(.title2)
